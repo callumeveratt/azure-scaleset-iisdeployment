b3b330d68a48f403340daacd3b3803246f0bd6ba
callumeveratt <80694407+callumeveratt@users.noreply.github.com>
2021-03-16 12:12:31 +0000

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,6 @@
-Start-IISCommitDelay
 New-WebAppPool -Name 'Expenses'
+Reset-IISServerManager -Confirm:$false
+Start-IISCommitDelay
 $expensesSite = New-IISSite -Name 'Expenses' -PhysicalPath 'C:\Products\Expenses' -BindingInformation "*:8088:"
 $expensesSite.Applications["/"].ApplicationPoolName = "Expenses"
 Stop-IISCommitDelay
