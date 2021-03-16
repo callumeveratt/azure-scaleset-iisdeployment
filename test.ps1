432ed860e6573d3fded7125f665ff1c4b6f0b757
callumeveratt <80694407+callumeveratt@users.noreply.github.com>
2021-03-16 12:03:39 +0000

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,5 @@
-New-IISSite -Name 'Expenses' -PhysicalPath 'C:\Products\Expenses' -BindingInformation "*:8088:"
+Start-IISCommitDelay
+New-WebAppPool -Name 'Expenses'
+$expensesSite = New-IISSite -Name 'Expenses' -PhysicalPath 'C:\Products\Expenses' -BindingInformation "*:8088:"
+$expensesSite.Applications["/"].ApplicationPoolName = "Expenses"
+Stop-IISCommitDelay
