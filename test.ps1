4b62deae08fd9b2d6d6625777962553c9fb0e583
callumeveratt <80694407+callumeveratt@users.noreply.github.com>
2021-03-16 12:31:29 +0000

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -2,6 +2,6 @@ Import-Module WebAdministration
 New-Item IIS:\AppPools\Expenses
 Reset-IISServerManager -Confirm:$false
 Start-IISCommitDelay
-$expensesSite = New-IISSite -Name 'Expenses' -PhysicalPath 'C:\Products\Expenses' -BindingInformation "*:8088:"
-$expensesSite.ApplicationPoolName = "Expenses"
+New-IISSite -Name 'Expenses' -PhysicalPath 'C:\Products\Expenses' -BindingInformation "*:8088:"
+Set-ItemProperty 'IIS:\Sites\Expenses' applicationPool Expenses
 Stop-IISCommitDelay
