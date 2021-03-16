01777333895b3816bc81b1413d9fa4161bb97cb8
callumeveratt <80694407+callumeveratt@users.noreply.github.com>
2021-03-16 12:39:22 +0000

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -3,5 +3,6 @@ New-Item IIS:\AppPools\Expenses
 Reset-IISServerManager -Confirm:$false
 Start-IISCommitDelay
 New-IISSite -Name 'Expenses' -PhysicalPath 'C:\Products\Expenses' -BindingInformation "*:8088:"
-Set-ItemProperty 'IIS:\Sites\Expenses' applicationPool Expenses
 Stop-IISCommitDelay
+
+Set-ItemProperty 'IIS:\Sites\Expenses' applicationPool Expenses
