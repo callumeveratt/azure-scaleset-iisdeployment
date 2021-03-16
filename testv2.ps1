4d382f7774de4ae4cb91cb39d183436e68f8efbe
callumeveratt <80694407+callumeveratt@users.noreply.github.com>
2021-03-16 19:00:35 +0000

    Update testv2.ps1

--- testv2.ps1
+++ testv2.ps1
@@ -1,8 +1,7 @@
 $getKeyVaultAssignedIdentityToken = Invoke-RestMethod -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -Method GET -Headers @{Metadata="true"}
 $keyVaultToken = $getKeyVaultAssignedIdentityToken.access_token
-$getapplicationPoolPassword = Invoke-RestMethod -Uri https://UKS-SPMT-KV-DEPLOY.vault.azure.net/secrets/applicationPoolPassword?api-version=2016-10-01 -Method GET -Headers @{Authorization="Bearer $keyVaultToken"}
-$applicationPoolPassword = $getapplicationPoolPassword.value
-$domainPassword = $applicationPoolPassword
+$getdomainPassword = Invoke-RestMethod -Uri https://UKS-SPMT-KV-DEPLOY.vault.azure.net/secrets/applicationPoolPassword?api-version=2016-10-01 -Method GET -Headers @{Authorization="Bearer $keyVaultToken"}
+$domainPassword = $getdomainPassword.value
 
 $expensesDeploymentScript = "C:\DeployTemp\deploy\expenses.ps1"
 .$expensesDeploymentScript
