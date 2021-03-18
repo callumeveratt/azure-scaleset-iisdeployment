bd359bd01d68da3355168d7d08844bf7733a8be5
callumeveratt <80694407+callumeveratt@users.noreply.github.com>
2021-03-18 10:33:11 +0000

    Update testv2.ps1

--- testv2.ps1
+++ testv2.ps1
@@ -1,19 +1,12 @@
 $getKeyVaultAssignedIdentityToken = Invoke-RestMethod -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -Method GET -Headers @{Metadata="true"}
 $keyVaultToken = $getKeyVaultAssignedIdentityToken.access_token
-$getdomainPassword = Invoke-RestMethod -Uri https://UKS-SPMT-KV-DEPLOY.vault.azure.net/secrets/applicationPoolPassword?api-version=2016-10-01 -Method GET -Headers @{Authorization="Bearer $keyVaultToken"}
-$domainPassword = $getdomainPassword.value
+$getLocalAdminPassword = Invoke-RestMethod -Uri https://UKS-SPMT-KV-DEPLOY.vault.azure.net/secrets/serverLocalAdminPassword?api-version=2016-10-01 -Method GET -Headers @{Authorization="Bearer $keyVaultToken"}
+$Password = $getLocalAdminPassword.value
 
-$domain = "selenityazure.com"
-$username =  "selenityazure\apppool-testing"
-$password = $domainPassword | ConvertTo-SecureString -asPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($username, $password)
-Add-Computer -DomainName $domain -Credential $credential -Force
+$serverDeploymentScript = "C:\DeployTemp\deploy\deployServer.ps1"
 
-$expensesDeploymentScript = "C:\DeployTemp\deploy\expenses.ps1"
-.$expensesDeploymentScript
-
-# Restart to complete domain join
-$RestartTaskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command &{Restart-Computer -Force}"
-$RestartTaskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds(3); $RestartTaskTrigger.EndBoundary = (Get-Date).AddSeconds(60).ToString('s')
-$RestartTaskSettings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter 00:00:30
-Register-ScheduledTask -Force -User SYSTEM -TaskName "Restart to Join Domain" -Action $RestartTaskAction -Trigger $RestartTaskTrigger -Settings $RestartTaskSettings
+# Set up server using tasks
+$ConfigTaskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File $serverDeploymentScript"
+$ConfigTaskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds(2); $ConfigTaskTrigger.EndBoundary = (Get-Date).AddSeconds(120).ToString('s')
+$ConfigTaskSettings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter 00:00:30
+Register-ScheduledTask -Force -User "SelAdmin" -Password $Password -TaskName "Configure Server" -Action $ConfigTaskAction -Trigger $ConfigTaskTrigger -Settings $ConfigTaskSettings
